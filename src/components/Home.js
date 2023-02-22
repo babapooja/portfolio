1a032dca6b8a0686dda84e51818fdd59302599e6
Pooja Baba <babapooja6@gmail.com>
2023-02-22 17:20:45 -0500

    pdated padding

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     <div
       name="home"
       className="h-screen w-full bg-gradient-to-b 
-      from-black via-black to-gray-800"
+      from-black via-black to-gray-800 pt-20"
     >
       <div
         className="max-w-screen-lg mx-auto flex flex-col
